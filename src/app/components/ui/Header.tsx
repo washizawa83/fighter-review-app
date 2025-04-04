f9e087b0cf7e10ce778eebd8d2a2c448392c2b16
washizawa83 <washizawa83@gmail.com>
2025-04-05 02:40:18 +0900

    ログイン画面作成

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,7 +1,10 @@
+import { getCurrentAuthUser, logout } from '@/app/service/auth'
 import Link from 'next/link'
 import { BasePageLayout } from '../layouts/BasePageLayout'
 
-export const Header = () => {
+export const Header = async () => {
+  const currentUser = await getCurrentAuthUser()
+
   return (
     <header className="h-12 w-full border-b border-dotted">
       <BasePageLayout>
@@ -10,9 +13,17 @@ export const Header = () => {
             <h1 className="text-3xl">Fighters Review</h1>
           </Link>
           <ul>
-            <Link href={'/pages/login'}>
-              <li className="font-rubikOne text-xl">Login</li>
-            </Link>
+            {currentUser ? (
+              <li className="font-rubikOne text-lg">
+                <button className="cursor-pointer" onClick={logout}>
+                  Logout
+                </button>
+              </li>
+            ) : (
+              <li className="font-rubikOne text-lg">
+                <Link href={'/pages/login'}>Login</Link>
+              </li>
+            )}
           </ul>
         </div>
       </BasePageLayout>
